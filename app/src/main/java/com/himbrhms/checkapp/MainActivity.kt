ee3e3dd7ae6dbaeb680093b008b76c07ed251b50
“Manuel <himbeerhummus@gmail.com>
2022-01-04 07:15:58 +0100

    Implementing Timber Logger

--- app/src/main/java/com/himbrhms/checkapp/MainActivity.kt
+++ app/src/main/java/com/himbrhms/checkapp/MainActivity.kt
@@ -8,11 +8,18 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.himbrhms.checkapp.ui.Navigation
+import com.himbrhms.checkapp.util.Logger
+import com.himbrhms.checkapp.util.className
 
 class MainActivity : ComponentActivity() {
 
+    companion object {
+        private val logger = Logger(this::class.className)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logger.debug("onCreate")
         setContent {
             Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
                 Navigation()
